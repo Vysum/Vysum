99472e7248c7b05a29bf26da08338a95f67f293a
DreadJaw <thedreadjaw@gmail.com>
2019-12-14 17:27:20 -0500

    Added more titlebar features to component

--- assets/components/titlebar.jsx
+++ assets/components/titlebar.jsx
@@ -1,7 +1,5 @@
 class titlebar extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+    constructor(props) { super(props); }
     render() {
         let classList = String('navbar navbar-expand-lg ');
         switch(this.props.theme) {
@@ -15,5 +13,57 @@ class titlebar extends React.Component {
         return (
             <div className={classList}>{this.props.children}</div>
         )
+    }
+}
+class titlebarBrand extends React.Component {
+    constructor(props) { super(props); }
+    render() {
+        <a className="navbar-brand" href="/">
+            {this.props.children}
+            <span class={this.props.classes}>{this.props.name}</span>
+        </a>
+    }
+}
+class titlebarToggle extends React.Component {
+    constructor(props) { super(props); }
+    render() {
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target={this.props.for}>
+            <span className="navbar-toggler-icon"></span>
+        </button>
+    }
+}
+class titlebarNavbar extends React.Component {
+    constructor(props) { super(props); }
+    render() {
+        let position = (this.props.position == 'left') ? 'mr-auto' : 'ml-auto';
+        <div className="collapse navbar-collapse" id={this.props.id}>
+            <ul className={'navbar-nav ' + position}>
+                {this.props.children}
+            </ul>
+        </div>
+    }
+}
+class titlebarLink extends React.Component {
+    constructor(props) { 
+        super(props); 
+        this.state = { active: false }
+    }
+    render() {
+        let isExternal = (this.props.link.startsWith('http')) ? true : false;
+        if(!isExternal) {
+            <li className="nav-item">
+                <a className="nav-link" href={this.props.link}>
+                    {this.props.children}
+                    <span> {this.props.label}</span>
+                </a>
+            </li>
+        } else {
+            <li className="nav-item">
+                <a className="nav-link" href={this.props.link} target="_blank">
+                    {this.props.children}
+                    <span> {this.props.label}</span>
+                </a>
+            </li>
+        }
     }
 }
