37cad6933e523827cca6cc74f12701498094ce24
DreadJaw <thedreadjaw@gmail.com>
2019-12-14 21:25:36 -0500

    Added better server startup features

--- source/index.ts
+++ source/index.ts
@@ -5,21 +5,33 @@ import helmet from 'helmet';
 import bodyParse from 'body-parser';
 import cookieParse from 'cookie-parser';
 
+import {Reporter} from './lib/reporter';
+
+import cdnRoute from './routes/cdn';
+
 const mainPort: number = 80;
 const securePort: number = 443;
 const appCore: express = express();
 const serverCore: http.Server = new http.Server(appCore);
 const io: ioSockets = ioSockets(serverCore);
+const report: Reporter = new Reporter();
+const startup: number = Date.now();
 
 appCore.use(helmet());
 appCore.use(cookieParse());
 appCore.use(bodyParse.urlencoded({ extended: false }));
 appCore.use(bodyParse.json());
 
-appCore.get('/', (request: any, response: any) => {
-    response.send({ status: 200 });    
-});
+appCore.use('/', cdnRoute);
+
+appCore.get('/_status/:code', function(request: any, response: any) {
+    report.info('status', `Response requested: ${request.params.code}`);
+    response.send(request.params.code);
+})
 
 serverCore.listen(mainPort, function() {
-    console.log('Now online.')
+    let bootdiff: string = ((Date.now() - startup) / 1000).toFixed(2);
+    report.success('server', `Server Started`);
+    report.success('server', `Started in: ${bootdiff}sec(s).`);
+    report.success('server', `Listening on PORT: ${mainPort}`);
 })
