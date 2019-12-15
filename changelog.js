829eb4140baefbd1eb82932126829f7fe250cd3b
DreadJaw <thedreadjaw@gmail.com>
2019-12-14 23:54:25 -0500

    Changelog creation script

---
+++ changelog.js
@@ -0,0 +1,18 @@
+const fs        = require('fs');
+const markdown  = require('markdown-tree');
+
+const readFile = src => fs.readFileSync(src, { encoding: 'utf-8' });
+
+const changelog = () => {
+	const changelog = readFile('CHANGELOG.md');
+	const tree = markdown(changelog);
+	const object = tree.children.map(c => ({
+		version: c.text,
+		changes: c.tokens.map(t => t.text).filter(x => x)
+	}));
+	const type = `{ version: string; changes: string[]; }[]`;
+	const code = `export const CHANGELOG: ${type} = ${JSON.stringify(object, null, 2)};\n`;
+	fs.writeFile('source/changelog.ts', code);
+};
+
+changelog();
